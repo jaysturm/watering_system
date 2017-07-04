b1d5c6967bde67ecb9b2900d6d6735cc6ea631c1
Jason Sturm <jsturm@kiosk.com>
2017-07-04 15:05:09 -0600

    adding express route methods organization

--- server.js
+++ server.js
@@ -1,26 +1,12 @@
 const express = require('express');
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send('You have reached the Wonka Factory API... please leave a message at the beep.');
-});
+var defaultRoute = require('./route_methods/index');
+var water = require('./route_methods/water');
+
+server.use('/', defaultRoute);
+server.post('/water', water);
 
 server.listen(5555, () => {
   console.log("Server running at http://127.0.0.1:5555/");
-})
-
-
-// // Load the http module to create an http server.
-// var http = require('http');
-
-// // Configure our HTTP server to respond with Hello World to all requests.
-// var server = http.createServer(function (request, response) {
-//   response.writeHead(200, {"Content-Type": "text/plain"});
-//   response.end("You have reached the Wonka Factory API... please leave a message at the beep.\n");
-// });
-
-// // Listen on port 5555, IP defaults to 127.0.0.1
-// server.listen(5555);
-
-// // Put a friendly message on the terminal
-// console.log("Server running at http://127.0.0.1:5555/");
+});
