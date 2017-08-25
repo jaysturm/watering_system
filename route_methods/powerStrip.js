59c1f93b72e7f65676bbbe697ba1b44a715d8100
Jason Sturm <jsturm@kiosk.com>
2017-08-25 17:38:39 -0600

    updated to read from file using fs

--- route_methods/powerStrip.js
+++ route_methods/powerStrip.js
@@ -2,19 +2,15 @@ var rpio = require('rpio');
 var express = require('express');
 var router = express.Router();
 var gpioUtil = require('../services/gpio.service');
-var sockets = require('../resources/sockets');
+var winston = require('winston');
+var sockets = new Socket[];
 
-// var sockets =
-// [
-//     { "pin": 3, "socket": 1, "state": false },
-//     { "pin": 5, "socket": 2, "state": false },
-//     { "pin": 7, "socket": 3, "state": false },
-//     { "pin": 11, "socket": 4, "state": false },
-//     { "pin": 13, "socket": 5, "state": false },
-//     { "pin": 15, "socket": 6, "state": false },
-//     { "pin": 19, "socket": 7, "state": false },
-//     { "pin": 21, "socket": 8, "state": false }
-// ];
+require('fs').readFile('..resources/sockets.json', 'utf8', (err, data) => {
+    if (err)
+       winston.error('Error getting contents of sockets json', err);
+
+    sockets = JSON.parse(data);
+});
 
 // middleware
 router.use((req, res, next) => {
@@ -51,7 +47,7 @@ router.post('/', (req, res) => {
             socket = sockets[socketIndex],
             pin = socket.pin;
 
-        console.log(`**** turning socket ${req.body.socket} ${onOff} ****`)
+        winston.info(`**** turning socket ${req.body.socket} ${onOff} ****`)
 
         // change socket power state
         rpio.write(pin, req.body.powerOn ? gpioUtil.turnOn : gpioUtil.turnOff);
@@ -59,12 +55,12 @@ router.post('/', (req, res) => {
         // set socket state
         socket.state = req.body.powerOn;
 
-        console.log(`**** finished turning power ${onOff} ****`);
+        winston.info(`**** finished turning power ${onOff} ****`);
 
         res.send(sockets);
         res.end();
     } catch (err) {
-        console.log('*** error changing socket power state ****', err);
+        winston.error('*** error changing socket power state ****', err);
         res.send(`error changing socket power state => ${err}`);
         res.end();
     }
