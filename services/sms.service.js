a0c730c00c8eafa670c24936da44f7f9324a7c7a
Jason Sturm <jsturm@kiosk.com>
2017-08-26 20:27:23 -0600

    logging in sms service

--- services/sms.service.js
+++ services/sms.service.js
@@ -2,6 +2,7 @@ var sid = 'AC800e0b4f00d2e58851c79dc5ef603aac';
 var tkn = '2d813bba8b02ab5bc292201ceacb50b8';
 var twilio = require('twilio');
 var client = new twilio(sid, tkn);
+var logger = require(`${__basedir}/logger`);
 
 module.exports = {
     sendSms: (to, body) => {
@@ -9,6 +10,8 @@ module.exports = {
             body: body,
             to: to,
             from: '+19703153614'
-        })
+        });
+
+        logger.info('sms sent');
     }
 };
